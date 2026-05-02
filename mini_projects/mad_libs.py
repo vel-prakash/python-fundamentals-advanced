186f81f27e4b7dc58328e2df89cd1bd4a61ae899
Vel Prakash <velprakashr08@gmail.com>
2026-05-02 13:34:05 +0530

    Add mad libs mini project using user input

--- mini_projects/mad_libs.py
+++ mini_projects/mad_libs.py
@@ -4,13 +4,13 @@
 
 # -------- Input --------
 adjective1 = input("Enter an adjective (description): ")
-noun1 = input("Enter a noun (person, place, thing): ")
+noun = input("Enter a noun (person, place, thing): ")
 adjective2 = input("Enter an adjective (description): ")
-verb1 = input("Enter a verb ending with 'ing': ")
+verb = input("Enter a verb ending with 'ing': ")
 adjective3 = input("Enter an adjective (description): ")
 
 # -------- Story --------
 print(f"Today I went to a {adjective1} zoo.")
-print(f"In an exhibit, I saw a {noun1}.")
-print(f"{noun1} was {adjective2} and {verb1}")
+print(f"In an exhibit, I saw a {noun}.")
+print(f"{noun} was {adjective2} and {verb}")
 print(f"I was {adjective3}!")
