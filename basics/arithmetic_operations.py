820e8c36e7a5d3493cd0de331f854fba19d57f04
Vel Prakash <velprakashr08@gmail.com>
2026-05-02 18:39:10 +0530

    Add arithmetic operations examples

---
+++ basics/arithmetic_operations.py
@@ -0,0 +1,35 @@
+# -------- Arithmetic Operations --------
+# This file demonstrates two methods:
+# 1. Standard operations (a = a + 1)
+# 2. Shorthand operations (a += 1)
+
+# -------- Initial Value --------
+friends = 0
+
+# -------- Addition --------
+friends = friends + 1
+friends += 1
+
+# -------- Subtraction --------
+friends = friends - 0.5
+friends -= 1
+
+# -------- Multiplication --------
+friends = friends * 3
+friends *= 3
+
+# -------- Division --------
+friends = friends /2
+friends /= 2
+
+# -------- More Operations --------
+friends += 0.875
+friends = friends **2
+friends **= 2
+ 
+print(friends)
+
+# -------- Modulus --------
+remainder = friends % 3
+
+print(remainder)
