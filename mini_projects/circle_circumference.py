262d2c0b677c820f1c29682dcd545e804ef3a7cf
Vel Prakash <velprakashr08@gmail.com>
2026-05-04 14:55:15 +0530

    Add circle circumference calculator using math module

---
+++ mini_projects/circle_circumference.py
@@ -0,0 +1,11 @@
+# -------- Import --------
+import math
+
+# -------- Input --------
+radius = float(input("Enter the radius of a circle: "))
+
+# -------- Calculation --------
+circumference = 2 * math.pi * radius
+
+# -------- Output --------
+print(f"The circumfernce of the circle is: {round(circumference, 2)} cm")
