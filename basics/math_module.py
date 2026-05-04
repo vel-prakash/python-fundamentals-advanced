a7356e0b5cde51e58a7fb045d5e06d85957f23cf
Vel Prakash <velprakashr08@gmail.com>
2026-05-04 13:58:43 +0530

    Add math module examples including sqrt, ceil, and floor functions

---
+++ basics/math_module.py
@@ -0,0 +1,21 @@
+# -------- Import --------
+import math
+
+# -------- Constants --------
+print(math.pi)
+print(math.e)
+
+# -------- Square Root --------
+x = 9
+sqrt_result = math.sqrt(x)
+print(sqrt_result)
+
+# -------- Ceiling --------
+y = 9.01
+ceil_result = math.ceil(y)
+print(ceil_result)
+
+# -------- Floor --------
+z = 9.9
+floor_result = math.floor(z)
+print(floor_result)
