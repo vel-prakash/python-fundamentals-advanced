9b390e17c26a07520cb93b50686b51a77859bca8
Vel Prakash <velprakashr08@gmail.com>
2026-05-04 13:39:32 +0530

    Add built-in functions examples for basic math operations

---
+++ basics/built_in_functions.py
@@ -0,0 +1,18 @@
+# -------- Variables --------
+x = 3.14
+y = -4
+z = 5
+
+# -------- Built-in Functions --------
+round_result = round(x)
+abs_result = abs(y)
+pow_result = pow(x, 3)
+max_result = max(x, y, z)
+min_result = min(x, y, z)
+
+# -------- Output --------
+print(round_result)
+print(abs_result)
+print(pow_result)
+print(max_result)
+print(min_result)
